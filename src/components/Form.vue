95d6ba9018723c50a3eab92e69d70dadffbac5f6
odysseusmax <royschristy@gmail.com>
2020-03-16 07:12:24 +0000

    redirect

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="text-white text-center p-8">
     <h1 class="font-serif text-3xl lg:text-4xl xl:text-5xl"> Shorty, The Url Shortener </h1>
-    <form class="p-1" @submit="submit">
+    <form @submit="submit">
         <input class="flex-auto bg-blue-600 hover:bg-white hover:text-black border-2 border-solid rounded-md border-blue-900 w-1/2 sm:w-3/4 h-12 font-mono text-center text-sm p-2" type="text" placeholder="Paste the URL you want to shorten" v-model="inputUrl" required/>
         <button class="flex-auto bg-blue-400 hover:bg-green-400 text-base md:text-lg w-1/2 sm:w-1/4 h-12 p-1 font-mono text-center rounded-md" type="submit"> shorten!</button>
     </form>
-    <div class="bg-red-600 w-full text-center" v-if="error">
+    <div class="bg-red-600 w-full text-center my-4 p-3 rounded-md" v-if="error">
         {{ errorMessage }}
     </div>
   </div>
